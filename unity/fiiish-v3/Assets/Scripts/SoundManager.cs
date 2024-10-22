6a87189d6a06b54006835dd9facf208753be5292
Andreas Neukoetter <andreas@omni-mad.com>
2024-10-22 19:14:00 +0200

    [S01E23] + Polish SFX in Unity.

--- unity/fiiish-v3/Assets/Scripts/SoundManager.cs
+++ unity/fiiish-v3/Assets/Scripts/SoundManager.cs
@@ -6,12 +6,77 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class SoundManager : MonoBehaviour
 {
+    class SoundConfig
+    {
+        private AsyncOperationHandle<AudioClip> handle;
+        private List<GameObject> instances = new List<GameObject>();
+        private int _amount = 0;
+        private string _key = "";
+        private Transform _parentTransform;
+        private float _lastTrigger = 0.0f;
+        private float _minSeperation = 0.0f;
+        public SoundConfig(Transform parentTransform, string key, int amount, float minSeperation = 0.0f)
+        {
+            _parentTransform = parentTransform;
+            _amount = amount;
+            _key = key;
+            _minSeperation = minSeperation;
+            handle = Addressables.LoadAssetAsync<AudioClip>(key);
+            handle.Completed += OnLoadCompleted;
+        }
+
+        public bool Trigger()
+        {
+            // audioSource.Play();
+            if (_minSeperation > 0.0f)
+            {
+                if (_lastTrigger > Time.time - _minSeperation)
+                {
+                    return false;
+                }
+            }
+            foreach (var instance in instances)
+            {
+                var ac = instance.GetComponent<AudioSource>();
+                if (!ac.isPlaying)
+                {
+                    _lastTrigger = Time.time;
+                    ac.Play();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Update()
+        {
+        }
+
+        private void OnLoadCompleted(AsyncOperationHandle<AudioClip> handle)
+        {
+            if( handle.Result != null)
+            {
+                for (var i = 0; i < _amount; ++i)
+                {
+                    var go = new GameObject();
+                    go.transform.SetParent(_parentTransform);
+                    go.name = $"SoundConfig_{_key}_{i}";
+                    var audioSource = go.AddComponent<AudioSource>();
+                    audioSource.clip = handle.Result;
+                    instances.Add(go);
+                }
+            } 
+        }
+    }
     private Dictionary<AudioEffectId, AsyncOperationHandle<AudioClip>> _audioClips = new Dictionary<AudioEffectId, AsyncOperationHandle<AudioClip>>();
+
+    private Dictionary<AudioEffectId, SoundConfig> _soundConfigs = new();
+    
     // Start is called before the first frame update
-    IEnumerator Start()
+    void Start()
     {
         Setup();
-        return Configure();
+        Configure();
     }
 
     void Setup()
@@ -19,45 +84,32 @@ public class SoundManager : MonoBehaviour
         
     }
 
-    IEnumerator Configure()
+    private void AddAudioEffect( AudioEffectId audioEffectId, string key, int amount, float minSeperation = 0.0f)
+    {
+        _soundConfigs[audioEffectId] = new SoundConfig( gameObject.transform, key, amount, minSeperation);
+    }
+    void Configure()
     {
 
-        {
-            var handle = Addressables.LoadAssetAsync<AudioClip>("sound_picked_coin");
-            _audioClips[AudioEffectId.CoinPickup] = handle;
-        }        
-        {
-            var handle = Addressables.LoadAssetAsync<AudioClip>("sound_fish_death");
-            _audioClips[AudioEffectId.FishDeath] = handle;
-        }        
-        return null;
+        AddAudioEffect( AudioEffectId.CoinPickup, "sound_picked_coin", 50, 0.025f);
+        AddAudioEffect( AudioEffectId.FishDeath, "sound_fish_death", 1);
     }
     // Update is called once per frame
     void Update()
     {
-        foreach (var audioSource in GetComponentsInChildren<AudioSource>())
+        foreach (var kvp in _soundConfigs)
         {
-            if (!audioSource.isPlaying)
-            {
-                Destroy(audioSource.gameObject);
-            }
+            kvp.Value.Update();
         }
     }
 
     public void TriggerSound(AudioEffectId audioEffectId)
     {
-        if (_audioClips.ContainsKey(audioEffectId))
+        if (_soundConfigs.ContainsKey(audioEffectId))
         {
-            var ach = _audioClips[audioEffectId];
-            if (ach.Result != null)
-            {
-                var ac = ach.Result;
-                var go = new GameObject();
-                var audioSource = go.AddComponent<AudioSource>();
-                audioSource.clip = ac;
-                audioSource.Play();
-                go.transform.parent = transform;
-            }
+            var soundConfig = _soundConfigs[audioEffectId];
+            
+            soundConfig.Trigger();
         }
     }
 }
