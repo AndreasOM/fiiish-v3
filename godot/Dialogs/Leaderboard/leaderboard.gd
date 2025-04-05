62b559313636bb014c69573549cf5815c43375df
Andreas Neukoetter <andreas@omni-mad.com>
2025-04-05 16:07:23 +0200

    [S03E12] + Add persistence to Leaderboards.

--- godot/Dialogs/Leaderboard/leaderboard.gd
+++ godot/Dialogs/Leaderboard/leaderboard.gd
@@ -1,5 +1,8 @@
 class_name Leaderboard
 
+const current_version: int = 1
+const oldest_supported_version: int = 1
+
 var _name: String = ""
 var _max_entries: int = 0
 var _entries: Array[ LeaderboardEntry ] = []
@@ -7,7 +10,24 @@ var _entries: Array[ LeaderboardEntry ] = []
 func _init( name: String, max_entries: int = 0 ):
 	self._name = name
 	self._max_entries = max_entries
+
+func serialize( s: Serializer ) -> bool:
+	var version: int = current_version;
+	version = s.serialize_u16( version )
+	if version < oldest_supported_version:
+		push_warning("Version not supported ", version)
+		return false
 	
+	_name = s.serialize_fixed_string( 32, _name )
+	_max_entries = s.serialize_u16( _max_entries )
+	
+	var leaderboard_entry_constructor = func() -> LeaderboardEntry:
+		return LeaderboardEntry.new( "", 0 )
+
+	_entries = s.serialize_array( _entries, leaderboard_entry_constructor )
+	
+	return false
+		
 func set_name( name: String ):
 	self._name = name
 	
