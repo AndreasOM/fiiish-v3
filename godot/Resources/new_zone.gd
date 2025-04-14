8ccb03ddabbaa2c290384b31136c66f2564d4d5c
Andreas Neukoetter <andreas@omni-mad.com>
2025-04-14 18:11:46 +0200

    [S03E14] : More zone loader cleanups.

--- godot/Resources/new_zone.gd
+++ godot/Resources/new_zone.gd
@@ -1,7 +1,6 @@
 extends Resource
 class_name NewZone
 
-
 var name: String = "[unknown]"
 var width: float = 0.0
 var height: float = 0.0
@@ -11,3 +10,44 @@ var layers: SerializableArray = SerializableArray.new(
 	func() -> NewZoneLayer:
 		return NewZoneLayer.new() 
 )
+
+func serialize( s: Serializer ) -> bool:
+	var magic: int = 0;
+	magic = s.serialize_u16( magic );
+	
+	if magic != 0x4f53:
+		push_error("Broken zone magic %04x" % magic )
+		return false
+
+	var version: int = 0;
+	version = s.serialize_u16( version );
+	
+	if version != 1:
+		push_error("Unsupported zone version %04x" % version )
+		return false
+
+	if !s.serialize_chunk_magic( [0x46, 0x49, 0x53, 0x48, 0x4e, 0x5a, 0x4e] ):
+		push_error( "Broken zone chunk magic")
+		return false
+
+	var flags = 0;
+	flags = s.serialize_u8( flags )
+
+	if flags != 0x45:	# 'E'
+		push_error( "Compression/flags '%c' not supported for zone." % flags)
+		return false
+	
+	var chunk_version: int = 0;
+	chunk_version = s.serialize_u32( chunk_version );
+#
+	if chunk_version != 2:
+		push_error("Unsupported zone chunk_version %04x" % chunk_version )
+		return false
+
+	self.name = s.serialize_fixed_string( 64, self.name )	
+	self.difficulty = s.serialize_u16( self.difficulty )
+	self.width = s.serialize_f32( self.width )
+	self.height = s.serialize_f32( self.height )
+	self.layers.serialize( s )
+	
+	return true
--- godot/Scripts/new_zone_resource_loader.gd
+++ godot/Scripts/new_zone_resource_loader.gd
@@ -20,69 +20,9 @@ func _handles_type(type: StringName) -> bool:
 	
 func _load(_path: String, original_path: String, _use_sub_threads: bool, _cache_mode: int) -> Variant:
 	# print("!!!!! Load ", original_path)
-	var n = NewZone.new()
-	
 	var s = Serializer.new()
 	s.load_file( original_path )
 
-	var magic: int = 0;
-	magic = s.serialize_u16( magic );
-	
-	if magic != 0x4f53:
-		push_error("Broken magic %04x" % magic )
-		return n
-
-	
-	var version: int = 0;
-	version = s.serialize_u16( version );
-	
-	if version != 1:
-		push_error("Unsupported version %04x" % version )
-		return n
-
-	if !s.serialize_chunk_magic( [0x46, 0x49, 0x53, 0x48, 0x4e, 0x5a, 0x4e] ):
-		push_error( "Broken chunk magic")
-		return false
-
-	var flags = 0;
-	flags = s.serialize_u8( flags )
-
-	if flags != 0x45:	# 'E'
-		push_error( "Compression/flags '%c' not supported." % flags)
-		return n
-	
-	var chunk_version: int = 0;
-	chunk_version = s.serialize_u32( chunk_version );
-#
-	if chunk_version != 2:
-		push_error("Unsupported chunk_version %04x" % chunk_version )
-		return n
-
-	var name: String = ""
-	name = s.serialize_fixed_string( 64, name )	
-	# print( "Name: ", name )
-	n.name = name
-	
-	var difficulty: int = 0
-	difficulty = s.serialize_u16( difficulty )
-	
-	n.difficulty = difficulty
-	
-	var size_x = 0.0
-	size_x = s.serialize_f32( size_x )
-	var size_y = 0.0
-	size_y = s.serialize_f32( size_y )
-	
-	n.width = size_x
-	n.height = size_y
-	
-	#print( "Difficulty: ", difficulty )
-	#print( "size_x    %f: " % size_x )
-	#print( "size_y    %f: " % size_y )
-		#self.difficulty = f.read_u16();
-		#self.size.x = f.read_f32();
-		#self.size.y = f.read_f32();
-
-	n.layers.serialize( s )
-		
+	var n = NewZone.new()
+	n.serialize( s )
 	return n
