6175e7c97baffc45e2ec5d9b83fa84ce1cd5b645
Andreas Neukoetter <andreas@omni-mad.com>
2025-04-17 15:38:45 +0200

    [S03E17] : Extract special coin handling into PickupManager.

--- godot/Scenes/game_manager.gd
+++ godot/Scenes/game_manager.gd
@@ -25,6 +25,7 @@ signal sound_triggered( SoundEffect )
 @export var game_zone: CollisionShape2D = null
 
 @export var entity_config_manager: EntityConfigManager = null
+@export var pickup_manager: PickupManager = null
 
 var _coin_rain_duration: float = 0.0
 var _coin_rain_coins_per_second: float = 0.0
@@ -34,11 +35,6 @@ var _coins: int = 0
 var _distance: float = 0.0
 var _paused: bool = true
 var current_zone_progress: float = 0.0
-
-var _special_coins_spawned: float = 0.0
-var _time_since_last_special_coin: float = 0.0
-var _special_coin_streak: int = 0
-var _special_coin_cooldown: float = 0.0
 
 var current_zone_width: float:
 	get:
@@ -103,17 +99,6 @@ func _process(delta: float) -> void:
 			self.spawn_zone()
 			pass
 	_distance += self.movement_x * delta
-
-	if _special_coins_spawned > 0.0:
-		_special_coins_spawned -= 1.0 * delta
-		_special_coins_spawned = max( 0.0, _special_coins_spawned )
-		# print("_special_coins_spawned %f" % _special_coins_spawned)
-		
-	if _time_since_last_special_coin > 0.0:
-		_time_since_last_special_coin = max( 0.0, _time_since_last_special_coin - delta )
-		
-	if _special_coin_cooldown > 0.0:
-		_special_coin_cooldown = max( 0.0, _special_coin_cooldown - delta )
 
 #	if Input.is_key_pressed(KEY_D):
 #		_distance += 100 * pixels_per_meter
@@ -210,40 +195,8 @@ func spawn_explosion( position: Vector2, fish: Fish ):
 			pickup.set_target_velocity( Vector2.ZERO, 1.0 * r )
 			pickup.disable_magnetic_for_seconds( 1.0 )
 	
-func _log_special_coin():
-	if _time_since_last_special_coin < 3.0:
-		_special_coin_streak += 1
-		# print("Coin streak %d" % _special_coin_streak)
-		if _special_coin_streak > 10:
-			_special_coin_cooldown = 10.0
-			_special_coin_streak = 0
-	
-func _pick_coin( fish: Fish ) -> EntityId.Id:
-	var luck_factor = max( 0.0, 1.0-0.1*_special_coins_spawned )
-	var rain_coin_probability = fish.get_skill_effect_value( SkillEffectIds.Id.RAIN_COIN_PROBABILITY, 0.0 )
-	var explosion_coin_probability = fish.get_skill_effect_value( SkillEffectIds.Id.EXPLOSION_COIN_PROBABILITY, 0.0 )
-
-	if _special_coin_cooldown > 0.0:
-		luck_factor = 0.0
-		
-	var r = randf()
-	if r < explosion_coin_probability*luck_factor:
-		_log_special_coin()
-		_special_coins_spawned += 1
-		_time_since_last_special_coin = 0.0
-		return EntityId.Id.PICKUPEXPLOSION
-
-	r = randf()
-	if r < rain_coin_probability*luck_factor:
-		_log_special_coin()
-		_special_coins_spawned += 1
-		_time_since_last_special_coin = 0.0
-		return EntityId.Id.PICKUPRAIN
-		
-	return EntityId.Id.PICKUPCOIN
-	
 func _instantiate_coin( fish: Fish ) -> Object:
-		var coin_entity_id = _pick_coin( fish )
+		var coin_entity_id = self.pickup_manager.pick_coin( fish )
 		var coin_ec = entity_config_manager.get_entry( coin_entity_id )
 		if coin_ec == null:
 			return null
--- godot/Scenes/pickup_manager.gd
+++ godot/Scenes/pickup_manager.gd
@@ -1,2 +1,52 @@
 extends Node
 class_name PickupManager
+
+var _special_coins_spawned: float = 0.0
+var _time_since_last_special_coin: float = 0.0
+var _special_coin_streak: int = 0
+var _special_coin_cooldown: float = 0.0
+
+func _process(delta: float) -> void:
+
+	if _special_coins_spawned > 0.0:
+		_special_coins_spawned -= 1.0 * delta
+		_special_coins_spawned = max( 0.0, _special_coins_spawned )
+		# print("_special_coins_spawned %f" % _special_coins_spawned)
+
+	if _time_since_last_special_coin > 0.0:
+		_time_since_last_special_coin = max( 0.0, _time_since_last_special_coin - delta )
+
+	if _special_coin_cooldown > 0.0:
+		_special_coin_cooldown = max( 0.0, _special_coin_cooldown - delta )
+
+func pick_coin( fish: Fish ) -> EntityId.Id:
+	var luck_factor = max( 0.0, 1.0-0.1*_special_coins_spawned )
+	var rain_coin_probability = fish.get_skill_effect_value( SkillEffectIds.Id.RAIN_COIN_PROBABILITY, 0.0 )
+	var explosion_coin_probability = fish.get_skill_effect_value( SkillEffectIds.Id.EXPLOSION_COIN_PROBABILITY, 0.0 )
+
+	if _special_coin_cooldown > 0.0:
+		luck_factor = 0.0
+		
+	var r = randf()
+	if r < explosion_coin_probability*luck_factor:
+		_log_special_coin()
+		_special_coins_spawned += 1
+		_time_since_last_special_coin = 0.0
+		return EntityId.Id.PICKUPEXPLOSION
+
+	r = randf()
+	if r < rain_coin_probability*luck_factor:
+		_log_special_coin()
+		_special_coins_spawned += 1
+		_time_since_last_special_coin = 0.0
+		return EntityId.Id.PICKUPRAIN
+		
+	return EntityId.Id.PICKUPCOIN
+
+func _log_special_coin():
+	if _time_since_last_special_coin < 3.0:
+		_special_coin_streak += 1
+		# print("Coin streak %d" % _special_coin_streak)
+		if _special_coin_streak > 10:
+			_special_coin_cooldown = 10.0
+			_special_coin_streak = 0
