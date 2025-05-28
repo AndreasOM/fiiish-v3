5e460b152bc514315dbe0e2632befc8de8340662
Andreas Neukoetter <andreas@omni-mad.com>
2025-05-28 17:47:29 +0200

    [S04E19] + Add Single Run Coins and Distance Achievements.

--- godot/Features/Achievements/Scripts/achievement_config.gd
+++ godot/Features/Achievements/Scripts/achievement_config.gd
@@ -2,6 +2,7 @@ class_name AchievementConfig
 extends Resource
 
 @export var id: String = ""
+@export var sort_index: int = -1
 @export var name: String = ""
 @export var icon: Texture2D = null
 @export var completion_condition: AchievementCondition = null
