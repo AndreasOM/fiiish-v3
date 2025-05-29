e86efecb0afdcd45fbc955343dd8550880e1b3cf
Andreas Neukoetter <andreas@omni-mad.com>
2025-05-29 15:20:33 +0200

    [S04E20] + Sort Achievements by sort_index.

--- godot/Features/Achievements/Scripts/achievement_config_manager.gd
+++ godot/Features/Achievements/Scripts/achievement_config_manager.gd
@@ -2,6 +2,7 @@ class_name AchievementConfigManager
 extends Node
 
 var _configs: Dictionary[ String, AchievementConfig ] = {}
+var _sorted_keys: Array[ String ] = []
 
 func _init() -> void:
 	self.reload()
@@ -27,8 +28,25 @@ func reload() -> void:
 
 	print( "Loaded %d achievement configs." % self._configs.size() )
 	
+	self._sort_keys()
+
+func _sort_keys() -> void:
+	var temp := []
+	for k in self._configs.keys():
+		var c = self._configs[ k ]
+		temp.push_back( [ k, c.sort_index ] )
+
+	temp.sort_custom(
+		func(a, b): return a[1] < b[1]
+	)
+	
+	self._sorted_keys.clear()
+	for e in temp:
+		self._sorted_keys.push_back( e[0] )
+		
 func get_keys() -> Array[ String ]:
-	return self._configs.keys()
+	# return self._configs.keys()
+	return self._sorted_keys
 	
 func get_config( id: String ) -> AchievementConfig:
 	return self._configs.get( id, null )
