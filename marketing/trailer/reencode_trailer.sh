014b7da2eb17f11aa46c598d53b8ee8e39b75855
Andreas Neukoetter <andreas@omni-mad.com>
2025-08-31 11:08:52 +0200

    [offstream] : Switch to faster trailer reencoding.

--- marketing/trailer/reencode_trailer.sh
+++ marketing/trailer/reencode_trailer.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-ffmpeg -i trailer-scripted.avi -c:v libx264 -preset veryslow -qp 0 -c:a copy trailer-scripted.mp4
+#ffmpeg -i trailer-scripted.avi -c:v libx264 -preset veryslow -qp 0 -c:a copy trailer-scripted.mp4
+ffmpeg -i trailer-scripted.avi -c:v libx264 -preset veryfast -crf 20 -c:a copy trailer-scripted.mp4
 
