f5bc70f6a01493f43e05c60a73d9868397a2d2b6
Andreas Neukoetter <andreas@omni-mad.com>
2025-09-15 19:19:20 +0200

    [S06E14] + Change Developer scene test font to actual game font.

--- godot/Features/Developer/font_size_test_label.gd
+++ godot/Features/Developer/font_size_test_label.gd
@@ -2,8 +2,9 @@ class_name FontSizeTestLabel
 extends Label
 
 @export var font_size: int = 8
-
+# const ALL_THINGS_PINK = preload("res://Fonts/all_things_pink.ttf")
 func _ready() -> void:
 	self.text = "Font Size %d" % self.font_size
-	#self.theme_override_font_sizes.font_size = self.font_size
 	self.add_theme_font_size_override("font_size", self.font_size)
+	#self.add_theme_font_override("font", ALL_THINGS_PINK)
+	self.theme_type_variation = "LabelHeader"
